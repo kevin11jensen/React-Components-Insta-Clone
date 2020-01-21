7b7fa409b1ead22e1df6e6d167b52ca58902872e
Kevin Jensen <jensenk28@yahoo.com>
2020-01-21 09:33:42 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ Look at each file to see where you need to pass props or add code
 import React from "react";
 import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
+import PostPage from "../src/components/PostsContainer/PostsPage";
+import SearchBar from "../src/components/SearchBar/SearchBarContainer";
 // import dummydata
+import dummyData from "./dummy-data";
+
 
 const App = () => {
   // set up state for the dummy data and pass to your PostsPage
