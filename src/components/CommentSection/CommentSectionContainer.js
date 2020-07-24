696d64c7c91cdc40a631364d881aba5d48f3da05
Kevin Jensen <kevinjensen1133@yahoo.com>
2020-07-24 09:11:52 -0700

    finished mvp

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -5,11 +5,15 @@ import Comment from "./Comment";
 import "./Comment.css";
 
 const CommentSection = props => {
+  // console.log('comment props', props);
   // Add state for the comments
-
+  const [comments, setComments] = useState(props.comments);
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
+      {comments.map(comment => (
+        <Comment comment = {comment} />
+      ))}
       <CommentInput />
     </div>
   );
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -4,10 +4,17 @@ import Post from "./Post";
 import "./Posts.css";
 
 // pass the data from App.js down as props then map through the data
-const PostsPage = () => {
+const PostsPage = (props) => {
+  // console.log('props', props.postData)
   return (
     <div className="posts-container-wrapper">
       {/* map through data here */}
+      {props.postData.map(post => (
+        <Post
+        key = {post.username}
+        post = {post}
+        />
+      ))}
     </div>
   );
 };
